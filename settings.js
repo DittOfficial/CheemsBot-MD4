6f53c40802a67adf0614f4626e0b7ab37fa9a5fe
DittBotz <108743628+DittOfficial@users.noreply.github.com>
2022-07-24 19:04:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,23 +37,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc" //ur owner name
-global.ytname = "YT: ARULL OFC" //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
+global.vcardowner = ['6281387292249'] //ur owner number
+global.ownername = "DitOfc" //ur owner name
+global.ytname = "YT: DITT OFC" //ur yt chanel name
+global.socialm = "GitHub: DittOfficial" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bomdy 
 global.owner = ['6285822347348']
 global.ownertag = '6285822347348' //ur tag number
-global.botname = 'ArullBotzMD' //ur bot name
-global.linkz = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/GdXv5LxKfDPHJ4WOH2rNu5" //ur website to be displayed
-global.botscript = 'https://youtu.be/k3QkeVdAMOs' //script link
+global.botname = 'Lxzyn-MD' //ur bot name
+global.linkz = "https://linktr.ee/dittstoree" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCPRTxLq04Qjffpno38xYJ5A" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/KlFmYB4Fr8j78S7edWoGBe' //script link
 global.reactmoji = "🗿" //ur menu react emoji
 global.themeemoji = "😩" //ur theme emoji
 global.packname = "Sticker by" //ur sticker watermark packname
-global.author = "ArullOfc" //ur sticker watermark author
+global.author = "DitOfc" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
